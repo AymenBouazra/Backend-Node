375d36d397011a4d44dde6f5fcc91811e1681df5
Bouazzanadhem <bouazzanadhem@gmail.com>
2021-04-22 17:11:23 +0200

    fix index and Authapi

--- passports/passport.js
+++ passports/passport.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const BearerStrategy = require('passport-http-bearer').Strategy;
-const User = require('../models/UserSchema');
+const Company = require('../models/CompanySchema');
 
 passport.use(new BearerStrategy(
     (token, done)=> {
       const decodedData = jwt.verify(token, process.env.JWT_SECRET);
       console.log(decodedData);
-      User.findById(decodedData.userId, (err, user)=> {
+      Company.findById(decodedData.userId, (err, user)=> {
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
         return done(null, user, { scope: 'all' });
