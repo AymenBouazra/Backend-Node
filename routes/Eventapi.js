d3fdc891842bd123f23cbf3ca780a511dcb43f7d
Bouazzanadhem <bouazzanadhem@gmail.com>
2021-04-23 16:23:58 +0200

    Add PASSPORT to secure APIs

--- routes/Eventapi.js
+++ routes/Eventapi.js
@@ -3,27 +3,27 @@ const router = express.Router();
 const passport = require('passport');
 const Event = require('../models/EventSchema');
 
-router.get('/events',async(req,res)=>{
+router.get('/events',passport.authenticate('bearer', { session: false }),async(req,res)=>{
     const events = await Event.find().populate('tags');
     res.json(events)
 });
 
-router.get('/events/:id',async(req,res)=>{
+router.get('/events/:id',passport.authenticate('bearer', { session: false }),async(req,res)=>{
     const eventId = await Event.findById(req.params.id);
     res.json(eventId);
 });
 
-router.post('/events',async(req,res)=>{
+router.post('/events',passport.authenticate('bearer', { session: false }),async(req,res)=>{
     const createdEvent = await Event.create(req.body);
     res.json(createdEvent);
 });
 
-router.put('/events/:id',async(req,res)=>{
+router.put('/events/:id',passport.authenticate('bearer', { session: false }),async(req,res)=>{
     const updateEvent = await Event.findByIdAndUpdate(req.params.id,req.body,{new: true});
     res.json(updateEvent);
 });
 
-router.delete('/events/:id',async(req,res)=>{
+router.delete('/events/:id',passport.authenticate('bearer', { session: false }),async(req,res)=>{
     const deleteEvent = await Event.findByIdAndDelete(req.params.id);
     res.json({message: 'delete seccussefuly'});
 })
