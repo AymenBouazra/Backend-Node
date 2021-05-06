60ae2cd1d8831a9ccc64b2414b59c73e1f2547c7
moez87 <harrabimoez87@gmail.com>
2021-05-06 12:22:17 +0100

    home api

--- routes/homeAPI.js
+++ routes/homeAPI.js
@@ -8,5 +8,9 @@ router.get('/all-events', async (req, res) => {
     const events = await Event.find().populate('tags');
     res.json(events)
 });
+router.get('/events/:id',async(req,res)=>{
+    const eventId = await Event.findById(req.params.id);
+    res.json(eventId);
+});
 
 module.exports = router;
